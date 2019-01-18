797c08cf97f33aa2a82a28cf5f568920c7c4e9b4
Syed Hussain Ather <shussainather@gmail.com>
2019-01-18 17:25:41 -0500

    cleaned up command

--- wrappers/rmats/wrapper.py
+++ wrappers/rmats/wrapper.py
@@ -4,11 +4,9 @@ treatment = ",".join(snakemake.input.b1)
 
 control = ",".join(snakemake.input.b2)
 
-outdir = "/".join(str(snakemake.output).split("/")[:2])
-
 shell("python2.7 rMATS.3.2.5/RNASeq-MATS.py "
     "-b1 {treatment} -b2 {control} "
     "-t {snakemake.params.reading} "
     "-len {snakemake.params.length} "
     "-gtf {snakemake.input.gtf} "
-    "-o {outdir} &> {snakemake.log}")
+    "-o rmats_out &> {snakemake.log}")
