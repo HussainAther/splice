eca6b9a52d1ee7eb9264755e7378d0b4538945cc
Syed Hussain Ather <shussainather@gmail.com>
2019-01-18 06:10:58 -0500

    reverted back to single command

--- wrappers/sashimiplot/wrapper.py
+++ wrappers/sashimiplot/wrapper.py
@@ -4,8 +4,8 @@ treatment = ",".join(snakemake.input.b1)
 
 control = ",".join(snakemake.input.b2)
 
-shell("rmats2sashimiplot "
-      "--b1 {treatment} --b2 {control} "
-      "--l1 \"experiment\" --l2 \"control\" "
-      "-o {snakemake.params.dir} -e {snakemake.input.rmats} "
-      "-t {snakemake.params.as_} &> {snakemake.log}")
+shell("""rmats2sashimiplot 
+         --b1 {treatment} --b2 {control} 
+         --l1 \"experiment\" --l2 \"control\" 
+         -o {snakemake.params.dir} -e {snakemake.input.rmats} 
+         -t {snakemake.params.as_} &> {snakemake.log}""")
