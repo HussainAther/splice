b67666f968bbef5b0cd2a71edcc755d319b7727d
Syed Hussain Ather <shussainather@gmail.com>
2019-01-18 05:11:46 -0500

    simplified

--- wrappers/sashimiplot/wrapper.py
+++ wrappers/sashimiplot/wrapper.py
@@ -4,10 +4,8 @@ treatment = ",".join(snakemake.input.b1)
 
 control = ",".join(snakemake.input.b2)
 
-shell("python rmats2sashimiplot/src/rmats2sashimiplot/rmats2sashimiplot.py "
+shell("rmats2sashimiplot "
       "--b1 {treatment} --b2 {control} "
       "--l1 \"experiment\" --l2 \"control\" "
       "-o {snakemake.params.dir} -e {snakemake.input.rmats} "
       "-t {snakemake.params.as_} &> {snakemake.log}")
-
-shell("touch {snakemake.output.event}")
