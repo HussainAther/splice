2c70691e29cd8265c218623cc8ffaeb8e3e48ad4
Syed Hussain Ather <shussainather@gmail.com>
2019-01-19 14:49:10 -0500

    reverted to original

--- wrappers/sashimiplot/wrapper.py
+++ wrappers/sashimiplot/wrapper.py
@@ -7,5 +7,5 @@ control = ",".join(snakemake.input.b2)
 shell("""rmats2sashimiplot 
          --b1 {treatment} --b2 {control} 
          --l1 \"experiment\" --l2 \"control\" 
-         -o {snakemake.params.dir} -e {snakemake.input.rmats} 
+         -o {snakemake.output.dir} -e {snakemake.input.rmats} 
          -t {snakemake.params.as_} &> {snakemake.log}""")
